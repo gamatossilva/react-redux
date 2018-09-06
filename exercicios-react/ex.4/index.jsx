aa148987222828e2c2fc6b78eae000450e7b71ae
Guilherme Augusto <guilherme_amsilva@hotmail.com>
2018-09-06 11:27:58 -0300

    More React exercises

--- exercicios-react/ex.4/index.jsx
+++ exercicios-react/ex.4/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Field from './field'
+import Component from './componente'
 
 ReactDOM.render(
-    <Field initialValue='Teste'/>, 
+    <Component value='Show!'/>, 
     document.getElementById('app')
 )
