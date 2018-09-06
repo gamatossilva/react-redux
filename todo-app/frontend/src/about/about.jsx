1ab39172a6eb81b64039e006e384bf4421524900
Guilherme Augusto <guilherme_amsilva@hotmail.com>
2018-09-06 18:36:53 -0300

    Finish frontend with react

---
+++ todo-app/frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós' />
+        <h2>Nossa história</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod recusandae molestiae beatae nam, quam, placeat voluptatem dignissimos consectetur a doloribus possimus sed ex eius in! Assumenda soluta aliquid veritatis id?</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolorem perferendis fugit ad, numquam placeat, illo doloremque voluptate natus deleniti eum magnam quasi corporis nesciunt eos illum inventore culpa itaque!</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique ratione dolore assumenda maxime cum, at ipsum neque nisi quaerat delectus earum harum, debitis recusandae possimus perspiciatis vitae corrupti voluptate odit?</p>
+    </div>
+)
