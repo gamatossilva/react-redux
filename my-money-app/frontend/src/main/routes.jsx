824ac30dc88d12002dc3602ce49ef7fcf8a092be
Guilherme Augusto <guilherme_amsilva@hotmail.com>
2018-09-11 18:51:39 -0300

    Implementing authorization on frontend

--- my-money-app/frontend/src/main/routes.jsx
+++ my-money-app/frontend/src/main/routes.jsx
@@ -4,11 +4,11 @@ import { Router, Route, Redirect, hashHistory, IndexRoute } from 'react-router'
 import Dashboard2 from '../dashboard2/dashboard2'
 import Dashboard from '../dashboard/dashboard'
 import BillingCycle from '../billingCycle/billingCycle'
-import App from './app'
+import AuthOrApp from './authOrApp'
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={App}>
+        <Route path='/' component={AuthOrApp}>
             <IndexRoute component={Dashboard} />
             <Route path='billingCycles' component={BillingCycle} />
         </Route>
